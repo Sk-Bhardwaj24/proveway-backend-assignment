8ccc83b18106d9a8ac097e6a7072cf1887dd3706
Saurabh Kumar Bhardwaj <91479687+Sk-Bhardwaj24@users.noreply.github.com>
2024-02-15 21:44:33 +0530

    Delete dist directory

--- dist/controllers/auth/auth.validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.googleSignin = void 0;
-const express_validation_1 = require("express-validation");
-exports.googleSignin = {
-    body: express_validation_1.Joi.object({
-        token: express_validation_1.Joi.string().required()
-    })
-};
--- dist/controllers/videos/videos.validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.downloadVideoValidation = void 0;
-const express_validation_1 = require("express-validation");
-exports.downloadVideoValidation = {
-    body: express_validation_1.Joi.object({
-        sourceFile: express_validation_1.Joi.string().required()
-    })
-};
